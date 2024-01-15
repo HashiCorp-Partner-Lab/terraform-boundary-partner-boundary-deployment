fa9d9076cc3eda3891acc762fc255473d767391a
dannyjknights <dannyjknights@gmail.com>
2024-01-15 09:28:30 +0000

    Added username-password output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "boundary_cluster_url" {
   value = hcp_boundary_cluster.partner_labs_boundary.cluster_url
 }
 
+output "boundary_admin_username" {
+  value = hcp_boundary_cluster.partner_labs_boundary.username
+}
+
+output "boundary_admin_password" {
+  value = hcp_boundary_cluster.partner_labs_boundary.password
+}
+
